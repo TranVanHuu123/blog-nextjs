bf0a416e4463b2469d5ee789f879622f8d75661e
huu <huu@DESKTOP-F0QJ6JJ>
2023-07-14 22:28:27 +0700

    update file

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,28 +1,33 @@
+"use client";
 import React from "react";
 import styles from "./styles.module.scss";
 import Link from "next/link";
 import Image from "next/image";
+import useSWR from "swr";
 
-async function getData() {
-  const res = await fetch("http://127.0.0.1:3000/api/posts", {
-    cache: "no-store",
-  });
+// async function getData() {
+//   const res = await fetch("http://127.0.0.1:3000/api/posts", {
+//     cache: "no-store",
+//   });
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
+//   if (!res.ok) {
+//     throw new Error("Failed to fetch data");
+//   }
 
-  return res.json();
-}
-export default async function Blog() {
-  const data = await getData();
+//   return res.json();
+// }
+export default function Blog() {
+  // const data = await getData();
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  const { data, mutate, error, isLoading } = useSWR("/api/posts", fetcher);
+
   return (
     <div className={styles.mainContainer}>
-      {data.map((item) => (
+      {data?.map((item) => (
         <Link
           href={`/blog/${item._id}`}
           className={styles.container}
-          key={item.id}
+          key={item._id}
         >
           <div className={styles.imageContainer}>
             <Image
